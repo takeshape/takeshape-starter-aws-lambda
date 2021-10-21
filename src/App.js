18307aadf2653f4d7380be0816ba92458347c5ff
S3Prototype <liuqahs15@gmail.com>
2021-10-21 17:12:42 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [productList, setProductList] = useState(null);
+
+  useEffect(() => {
+    (async ()=>{
+      try {
+        const result = await fetch(process.env.REACT_APP_AWS_ENDPOINT);
+        const resultJSON = await result.json();
+        setProductList(resultJSON.products);
+        console.log(resultJSON);
+      } catch (err){
+        console.log('Failed to get data from AWS lambda function', err);
+      }
+    })();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main style={{width:'50%',margin:'auto', display:'flex', flexDirection:'column'}}>
+      {productList && productList.map(product=>(
+        <div key={product._id} style={{display:'flex', flexDirection:'column'}}>
+          <img alt={product.name} style={{maxWidth:'800px'}} src={product.image.sourceUrl}></img>
+          <h2>{product.name}</h2>
+          <b>{product.price}</b>
+        </div>
+      ))}
+    </main>
   );
 }
 
